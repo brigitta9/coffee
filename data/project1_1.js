c53e36a503420640a70de9c7e1f41f9076d0b24c
brigitta9 <75415492+brigitta9@users.noreply.github.com>
2020-12-03 15:40:55 +0700

    Add files via upload

---
+++ data/project1_1.js
@@ -0,0 +1,8 @@
+var json_project1_1 = {
+"type": "FeatureCollection",
+"name": "project1_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.901419300548142, -6.112362330125095 ] } }
+]
+}
